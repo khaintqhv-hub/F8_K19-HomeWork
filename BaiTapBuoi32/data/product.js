b9f9fcc29074704f85e83e6f3b026b65d6ca7fd2
khaintqhv-hub <khaintqhv@fullstack.edu.vn>
2026-07-27 19:39:47 +0700

    Add files via upload

---
+++ BaiTapBuoi32/data/product.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 17 Pro Max 256GB | Chính hãng",
+    brand: "Apple",
+    image:
+      "https://picsum.photos/300/300?random=1",
+    discount: "Giảm 5%",
+    price: "35.990.000đ",
+    oldPrice: "37.990.000đ",
+    rating: 5,
+    delivery: "2 Giờ"
+  },
+
+  {
+    id: 2,
+    name: "Samsung Galaxy S26 Ultra 5G 12GB 256GB",
+    brand: "Samsung",
+    image:
+      "https://picsum.photos/300/300?random=2",
+    discount: "Giảm 16%",
+    price: "30.890.000đ",
+    oldPrice: "34.990.000đ",
+    rating: 5,
+    delivery: "2 Giờ"
+  },
+
+  {
+    id: 3,
+    name: "OPPO Find X9 Ultra 12GB 512GB",
+    brand: "OPPO",
+    image:
+      "https://picsum.photos/300/300?random=3",
+    discount: "Giảm 2%",
+    price: "48.990.000đ",
+    oldPrice: "50.000.000đ",
+    rating: 5,
+    delivery: "2 Giờ"
+  },
+
+  {
+    id: 4,
+    name: "Samsung Galaxy Z Fold7 12GB 256GB",
+    brand: "Samsung",
+    image:
+      "https://picsum.photos/300/300?random=4",
+    discount: "Giảm 1%",
+    price: "41.990.000đ",
+    oldPrice: "43.000.000đ",
+    rating: 5,
+    delivery: "2 Giờ"
+  },
+
+  {
+    id: 5,
+    name: "Samsung Galaxy S26 5G 12GB 256GB",
+    brand: "Samsung",
+    image:
+      "https://picsum.photos/300/300?random=5",
+    discount: "Giảm 17%",
+    price: "21.490.000đ",
+    oldPrice: "25.000.000đ",
+    rating: 5,
+    delivery: "2 Giờ"
+  },
+
+  {
+    id: 6,
+    name: "Xiaomi Redmi Note 14 Pro Plus 5G",
+    brand: "Xiaomi",
+    image:
+      "https://picsum.photos/300/300?random=6",
+    discount: "Giảm 21%",
+    price: "8.490.000đ",
+    oldPrice: "10.990.000đ",
+    rating: 5,
+    delivery: "2 Giờ"
+  }
+];
+
+
+export default products;
